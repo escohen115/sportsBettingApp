59b7392f9c8792171fcb56908188158fbe2d929f
simon <escohen115@gmail.com>
2022-11-12 07:11:22 -0500

    secrets

--- sports_betting_app_frontend/src/App.js
+++ sports_betting_app_frontend/src/App.js
@@ -3,14 +3,15 @@ import React, {useEffect, useState} from 'react'
 
 function App() {
 
-const [games,setGames] = useState([])
+
+const [games,setGames] = useState({response:[]})
 
 useEffect(() => {
 
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '2dd546e72dmsh540546b64658ae0p141ce0jsnbfa9db400034',
+		'X-RapidAPI-Key': `${process.env.REACT_APP_API_KEY}`,
 		'X-RapidAPI-Host': 'api-basketball.p.rapidapi.com'
 	}
 };
@@ -21,15 +22,25 @@ fetch('https://api-basketball.p.rapidapi.com/games?date=2019-11-26', options)
 	.catch(err => console.error(err));
 },[])
 
+function handleSubmit(){
 
-
+}
 
   return (
     <div className="App">
-      <form>
-        
-      </form>
-      hello {JSON.stringify(games.response[0])}
+		<form >
+			<label>Select Sport
+				<select>
+						<option value="grapefruit">Grapefruit</option>
+						<option value="lime">Lime</option>
+						<option selected value="coconut">Coconut</option>
+						<option value="mango">Mango</option>
+				</select>
+			</label>
+			<input type="submit" value="Submit" />
+		</form>
+      
+      {/* hello {JSON.stringify(games.response[0])} */}
     </div>
   );
 }
